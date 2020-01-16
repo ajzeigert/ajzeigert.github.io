778cb61c58916337723651427f91c0246b4d34d4
ajzeigert <ajzeigert@gmail.com>
2020-01-16 11:20:58 -0800

    updates some react stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,12 @@ const styles = {
 const Home = () => (
   <div>
     <h2>Home</h2>
+    <p>This site provides examples of my web development and print design work.</p>
+    <h3>Brief History</h3>
+    <p>I studied journalism in college with a specialization in design and information graphics.</p>
+    <p>I worked at a newspaper for 10-ish years as a designer and graphics reporter. During that time I also started making interactive infographics for the web.</p>
+    <p>I then joined an engineering firm as a full-time developer.</p>
+    <p>In addition to my primary software engineering gig, I also design magazines and create infographics for print and the web.</p>
   </div>
 );
 
--- src/pages/Web.js
+++ src/pages/Web.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import injectSheet from 'react-jss'
-import Map from 'pigeon-maps'
+// import Map from 'pigeon-maps'
 
 const styles = {
 
@@ -9,14 +9,21 @@ const styles = {
 const Web = () => (
   <div>
     <h2>Web</h2>
-	<h3>Map styles</h3>
+	<p><em>Much of the web design work I do is for applications used internally by my company. I'll provide URLs where I can, but for projects that require a login, I'll provide some screenshots.</em></p>
+	<h3>Birdies Cafe</h3>
+	<h4>Homepage: <a href='http://birdiescafebend.com/'>http://birdiescafebend.com/</a></h4>
+	<p>Designed, built and maintained a simple website for a local restaurant. I also created the logo!</p>
+	<h3>Spectrum Editing</h3>
+	<h4>Homepage: <a href='http://spectrumediting.com'>http://spectrumediting.com</a></h4>
+	<p>Designed and built a simple home page for a personal business.</p> 
+	{/* <h3>Map styles</h3>
 	<Map
 		center={[44.053, -123.112]}
 		zoom={12}
 		width={1000}
 		height={600}
 		provider={(x,y,z) => `https://api.mapbox.com/styles/v1/ajzeigert/cigtoceeo000la9lwz194vo7a/tiles/256/${z}/${x}/${y}@2x?access_token=pk.eyJ1IjoiYWp6ZWlnZXJ0IiwiYSI6IldLdVhKN1UifQ.43CCALwNLBzVybtPFvcaJQ`}
-	></Map>
+	></Map> */}
   </div>
 );
 
